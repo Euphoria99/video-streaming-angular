225144117ff3009a302ddda0c136c436a3119302
Euphoria99 <363pavanbhat@gmail.com>
2024-09-29 14:00:00 +0100

    Added Video Player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  private apiUrl = environment.apiUrl;
   title = 'VideoStreamingApp';
+
+  videoId: any = "25383b6b-3096-41d8-8ace-7bdaf344dee9";
+
+  videoUrl:string;
+
+  constructor(){
+   this.videoUrl = `${this.apiUrl}/videos/stream/${this.videoId}`;
+  }
 }
